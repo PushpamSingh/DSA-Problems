0f6bc1a4c0f98802570fe8f444a2dc503edc34ed
PushpamSingh <pushpamsingh204@gmail.com>
2024-11-28 08:18:58 +0530

    bst

--- tree/BinarySearchTree/CreateBST.cpp
+++ tree/BinarySearchTree/CreateBST.cpp
@@ -33,6 +33,61 @@ void levelOrderTraversal(Node* root){
     }
 
 }
+Node* minVal(Node* root){
+    Node* temp=root;
+    while(temp->left!=NULL){
+        temp=temp->left;
+    }
+    return temp;
+}
+Node* maxVal(Node* root){
+    Node* temp=root;
+    while(temp->right!=NULL){
+        temp=temp->right;
+    }
+    return temp;
+}
+Node* DeleteNodeBST(Node* root,int val){
+    if(root==NULL){
+        return root;
+    }
+    if(root->data==val){
+        //0 child
+            if(root->left==NULL && root->right==NULL){
+                delete root;
+                return NULL;
+            }
+        //1 child
+            if(root->left!=NULL && root->right==NULL){
+                Node* temp=root->left;
+                delete root;
+                return temp;
+            }
+           
+            if(root->left==NULL && root->right!=NULL){
+                Node* temp=root->right;
+                delete root;
+                return temp;
+            }
+            
+        //2 child
+        if(root->left!=NULL && root->right!=NULL){
+            int min=minVal(root->right)->data;
+            root->data=min;
+            root->right=DeleteNodeBST(root->right,val);
+            return root;
+
+        }
+    }
+    else if(root->data>val){
+        root->left=DeleteNodeBST(root->left,val);
+        return root;
+    }
+    else{
+        root->right=DeleteNodeBST(root->right,val);
+        return root;
+    }
+}
 Node* insertIntoBSt(Node* root,int data){
     if(root==NULL){
         root=new Node(data);
@@ -64,5 +119,10 @@ int main(){
     cout<<"Printing data";
 
     levelOrderTraversal(root);
+    cout<<endl<<"Min val is: "<<minVal(root)->data<<endl;
+    cout<<"Min val is: "<<maxVal(root)->data<<endl;
+    DeleteNodeBST(root,7);
+    levelOrderTraversal(root);
+
 
 }
